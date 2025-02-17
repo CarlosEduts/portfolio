da7535d4e1eb7bfea44f7074198ea2271d6c69df
Carlos Eduardo <cedut.dev@gmail.com>
2025-02-17 20:18:01 -0300

    feat: Add dackground particle component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,17 @@
+import { Particles } from "@/components/ui/particles";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
   const t = useTranslations("Home");
   return (
-    <div>
-      <h1>{t("title")}</h1>
+    <div className="dark">
+      <Particles
+        className="absolute inset-0"
+        quantity={100}
+        ease={80}
+        color={"#000"}
+        refresh
+      />
     </div>
   );
 }
